c08694d578708fa71686ccbb4c5fe66b4184e0ed
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2019-05-21 17:55:37 +0200

    fix: auto bind router to location only in browser

--- src/router.js
+++ src/router.js
@@ -90,7 +90,7 @@ export default class Router {
         return {
             base: '/',
             dispatch: true,
-            bind: true,
+            bind: !!DOCUMENT,
             parser: EXPRESS_PARSER,
             triggerHashChange: true,
         };
